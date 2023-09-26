6aaaa91dbc4e73735f1059ccbefe663fc64e808d
KitSarmiento <ksarmiento020@gmail.com>
2023-09-26 18:56:47 -0400

    created a table under the seed.sql for the role data including the title, salary and department id.

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,9 +4,17 @@ VALUES  ('Engineering'),
         ('Finance'),
         ('Human Resources'),
         ('Legal'),
-        ('Sales'),
+        ('Sales');
 
 -- Roles --
-
+INSERT INTO role (title, salary, department_id)
+VALUES  ('Director of Engineer', 115000, 1),
+        ('Lead Engineer', 85000, 1),
+        ('Accountant', 95000, 2),
+        ('Finance Staff', 80000, 2),
+        ('Human Resources Staff', 93000, 3),
+        ('Corporate Lawyer', 155000, 4),
+        ('Lead Legal Team', 90000, 4),
+        ('Customer Service', 75000, 5);
 
 -- Employee Table --
