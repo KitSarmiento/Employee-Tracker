ac2310a562a45f55b05e4c71fed9bb3453604337
KitSarmiento <ksarmiento020@gmail.com>
2023-10-02 17:51:02 -0400

    installed an express package and added a middleware to the code.

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 // Connect to database
 const db = mysql.createConnection(
   {
