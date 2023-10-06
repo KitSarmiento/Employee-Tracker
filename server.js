6218f41d1860c356b2df1e7b94a2bd9aff1a060a
KitSarmiento <ksarmiento020@gmail.com>
2023-10-05 20:20:51 -0400

    fixed bug in Line 30 by adding a comma.

--- server.js
+++ server.js
@@ -21,3 +21,59 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the employeeTracker_db database.`)
 );
+
+function mainTracker() {
+  inquirer.prompt(
+    [
+      {
+        type: "List",
+        name: "action",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Update Employee Role",
+          "Remove an employee",
+          "Exit",
+        ],
+      },
+    ].then((answers) => {
+      switch (answers.action) {
+        case "View all departments":
+          viewAllDepartments();
+          break;
+
+        case "View all roles":
+          viewAllRoles();
+          break;
+
+        case "View all employees":
+          viewAllEmployees();
+          break;
+
+        case "Add a department":
+          addADepartment();
+          break;
+
+        case "Add a role":
+          addARole();
+          break;
+
+        case "Update Employee Role":
+          updateAEmployeeRole();
+          break;
+
+        case "Remove an employee":
+          removeAnEmployee();
+          break;
+
+        case "Exit":
+          connection.end();
+          break;
+      }
+    })
+  );
+}
